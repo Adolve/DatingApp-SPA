83c707ca461847202f4a5d8ee20572da01ee9252
Soufiane Belmir <belmir.soufiane@gmail.com>
2019-01-07 05:27:19 -0600

    working in login

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../_services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,11 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  model : any = {};
+  model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
+
+  login() {
+    this.authService.login(this.model).subscribe(next =>{
+      console.log('good');
+    }, error => {
+      console.log('error');
+    });    
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
+
 
 }
